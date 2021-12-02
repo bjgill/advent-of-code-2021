ec079bba0bac9628cfa43bd450ea83726c120113
Benjamin Gill <git@bjgill.net>
2021-12-02 22:55:02 +0000

    Add function for day 1.2
    We just need to compare the first and last member of the window.

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
-fn get_single_increase_count(mut readings: Vec<u32>) -> u32{
-    let mut readings= readings.iter_mut();
+fn get_single_increase_count(mut readings: Vec<u32>) -> u32 {
+    let mut readings = readings.iter_mut();
 
     let mut previous_reading = readings.next().unwrap();
     let mut increases = 0;
@@ -15,11 +15,35 @@ fn get_single_increase_count(mut readings: Vec<u32>) -> u32{
     increases
 }
 
+/// The 3-long sliding increase comparison is equivalent to comparing single values 3 apart.
+fn get_triple_increase_count(readings: Vec<u32>) -> u32 {
+    let reading_windows = readings.windows(4);
+    let mut increases = 0;
+
+    for window in reading_windows {
+        println!("{:?}", window);
+        if window.last() > window.first() {
+            increases += 1;
+        }
+    }
+    increases
+}
+
 fn main() {
     let filename = "data/day1.1.txt";
 
     let input = fs::read_to_string(filename).unwrap();
-    let depth_readings: Vec<u32> = input.split("\n").map(|i| i.parse::<u32>().unwrap()).collect();
+    let depth_readings: Vec<u32> = input
+        .split("\n")
+        .map(|i| i.parse::<u32>().unwrap())
+        .collect();
 
-    println!("Single increase count: {}", get_single_increase_count(depth_readings.clone()));
+    println!(
+        "Single increase count: {}",
+        get_single_increase_count(depth_readings.clone())
+    );
+    println!(
+        "Triple increase count: {}",
+        get_triple_increase_count(depth_readings.clone())
+    );
 }
