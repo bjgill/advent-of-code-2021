192a7f2ad55f5b176c940734f53715c0aa823b46
Benjamin Gill <git@bjgill.net>
2021-12-03 23:45:48 +0000

    Solve day 3 part 2
    Not very elegant - just brute force.

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,4 +1,4 @@
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 struct Report<const L: usize>([bool; L]);
 
 fn parse_reports<const L: usize>(input: &str) -> Vec<Report<L>> {
@@ -35,13 +35,13 @@ fn calculate_e_g<const L: usize>(reports: Vec<Report<L>>) -> (u32, u32) {
         .map(|total| total * 2 >= report_count)
         .collect();
 
-    let epsilon = bits_to_u32(most_common_bits);
+    let epsilon = bits_to_u32(&most_common_bits);
     let gamma = 2u32.pow(L as u32) - 1 - epsilon;
 
     (epsilon, gamma)
 }
 
-fn bits_to_u32(bits: Vec<bool>) -> u32 {
+fn bits_to_u32(bits: &[bool]) -> u32 {
     bits.iter()
         .zip((0..bits.len()).rev())
         .fold(0, |total, (bit, order)| {
@@ -49,11 +49,72 @@ fn bits_to_u32(bits: Vec<bool>) -> u32 {
         })
 }
 
+fn filter_most_common_value_for_position<const L: usize>(
+    reports: Vec<Report<L>>,
+    position: usize,
+) -> Vec<Report<L>> {
+    let total_for_column = reports
+        .iter()
+        .fold(0, |acc, report| acc + report.0[position] as u32);
+    let most_common_value = total_for_column * 2 >= reports.len() as u32;
+
+    reports
+        .into_iter()
+        .filter(|report| report.0[position] == most_common_value)
+        .collect()
+}
+
+fn filter_least_common_value_for_position<const L: usize>(
+    reports: Vec<Report<L>>,
+    position: usize,
+) -> Vec<Report<L>> {
+    let total_for_column = reports
+        .iter()
+        .fold(0, |acc, report| acc + report.0[position] as u32);
+    let most_common_value = total_for_column * 2 >= reports.len() as u32;
+
+    reports
+        .into_iter()
+        .filter(|report| report.0[position] != most_common_value)
+        .collect()
+}
+
+fn get_o2_generator_rating<const L: usize>(reports: Vec<Report<L>>) -> u32 {
+    let mut reports = reports;
+    for i in 0..L {
+        reports = filter_most_common_value_for_position(reports, i);
+
+        if reports.len() == 1 {
+            break;
+        }
+    }
+
+    bits_to_u32(&reports[0].0)
+}
+
+fn get_co2_scrubber_rating<const L: usize>(reports: Vec<Report<L>>) -> u32 {
+    let mut reports = reports;
+    for i in 0..L {
+        reports = filter_least_common_value_for_position(reports, i);
+
+        if reports.len() == 1 {
+            break;
+        }
+    }
+
+    bits_to_u32(&reports[0].0)
+}
+
 fn main() {
     let input = std::fs::read_to_string("data/day3.txt").unwrap();
+    let reports = parse_reports::<12>(&input);
 
-    let (epsilon, gamma) = calculate_e_g(parse_reports::<12>(&input));
+    let (epsilon, gamma) = calculate_e_g(reports.clone());
     println!("{:?}", epsilon * gamma);
+
+    let o2 = get_o2_generator_rating(reports.clone());
+    let co2 = get_co2_scrubber_rating(reports);
+    println!("{:?}", o2 * co2);
 }
 
 #[cfg(test)]
@@ -80,6 +141,38 @@ mod tests {
     #[test]
     fn test_power_report() {
         assert_eq!(calculate_e_g::<5>(get_test_reports()), (22, 9));
+    }
+
+    #[test]
+    fn test_most_common() {
+        assert_eq!(
+            filter_most_common_value_for_position(
+                vec![Report([true, false]), Report([false, false])],
+                0
+            ),
+            vec![Report([true, false])]
+        );
+    }
+
+    #[test]
+    fn test_o2_generator() {
+        assert_eq!(get_o2_generator_rating(get_test_reports()), 23);
+    }
+
+    #[test]
+    fn test_least_common() {
+        assert_eq!(
+            filter_least_common_value_for_position(
+                vec![Report([true, false]), Report([false, false])],
+                0
+            ),
+            vec![Report([false, false])]
+        );
+    }
+
+    #[test]
+    fn test_co2_scrubber() {
+        assert_eq!(get_co2_scrubber_rating(get_test_reports()), 10);
     }
 
     #[test]
@@ -112,13 +205,13 @@ mod tests {
     #[test]
     fn test_bits_to_u32() {
         assert_eq!(
-            bits_to_u32(vec![
+            bits_to_u32(&[
                 false, false, false, false, false, false, false, false, false, false, false, true
             ]),
             1
         );
         assert_eq!(
-            bits_to_u32(vec![
+            bits_to_u32(&[
                 false, false, false, false, false, false, false, false, false, false, true, true
             ]),
             3
