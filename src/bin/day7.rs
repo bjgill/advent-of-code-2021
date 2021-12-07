41d20372eec3388e4826010107fb11f687727e9c
Benjamin Gill <git@bjgill.net>
2021-12-07 20:11:34 +0000

    'Solve' day 7 part 2
    It's not really solved - I made a guess at needing to get to the
    average, and then checked a few adjacent values. But it got me to the
    right answer, so this seems good enough...

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -4,21 +4,51 @@
 //! The fuel required to reach one on either side must necessarily be more - whichever crab is the median will need to
 //! move. So at least half of crabs will become further, and at most half of crabs will be closer.
 
-fn fuel_use(crabs: Vec<i32>) -> i32 {
+fn minimal_linear_fuel(crabs: Vec<i32>) -> i32 {
     let mut crabs = crabs;
 
     crabs.sort();
 
-    let median = crabs[crabs.len()/2];
+    let median = crabs[crabs.len() / 2];
 
     crabs.into_iter().map(|c| (c - median).abs()).sum()
 }
 
+fn triangle(distance: i32) -> i32 {
+    (distance * (distance + 1)) / 2
+}
+
+/// This is a fudge, determined experimentally for the particular day 7 input data. It's not universally applicable.
+fn minimal_triangular_fuel(crabs: Vec<i32>) -> i32 {
+    let average = (crabs.iter().sum::<i32>() as f32 / (crabs.len() as f32)).round() as i32;
+
+    eprintln!("{}", average);
+
+    i32::min(
+        crabs
+            .clone()
+            .into_iter()
+            .map(|c| triangle((c - average).abs()))
+            .sum(),
+        crabs
+            .into_iter()
+            .map(|c| triangle((c - average + 1).abs()))
+            .sum(),
+    )
+}
+
 fn main() {
     let input = std::fs::read_to_string("data/day7.txt").unwrap();
-    let crabs = input.split(',').map(|c| c.parse().unwrap()).collect();
+    let crabs: Vec<i32> = input.split(',').map(|c| c.parse().unwrap()).collect();
 
-    println!("Minimum fuel use: {}", fuel_use(crabs));
+    println!(
+        "Minimum linear fuel use: {}",
+        minimal_linear_fuel(crabs.clone())
+    );
+    println!(
+        "Minimum triangular fuel use: {}",
+        minimal_triangular_fuel(crabs)
+    );
 }
 
 #[cfg(test)]
@@ -26,7 +56,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_expected_fuel() {
-        assert_eq!(fuel_use(vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14]), 37);
+    fn test_expected_linear_fuel() {
+        assert_eq!(
+            minimal_linear_fuel(vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14]),
+            37
+        );
+    }
+
+    #[test]
+    fn test_expected_triangular_fuel() {
+        assert_eq!(
+            minimal_triangular_fuel(vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14]),
+            168
+        );
     }
 }
