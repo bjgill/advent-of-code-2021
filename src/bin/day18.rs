81adfa0b65921b7332a98d441beab4e7a3973ae5
Benjamin Gill <git@bjgill.net>
2021-12-19 00:51:31 +0000

    Add support for exploding

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -7,15 +7,29 @@ use nom::{
     Finish, IResult,
 };
 
+use std::fmt;
 use std::str::FromStr;
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Clone)]
 enum Element {
     Pair {
         left: Box<Element>,
         right: Box<Element>,
     },
     Number(u32),
+}
+
+impl fmt::Debug for Element {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Element::Pair { left, right } => {
+                f.debug_list().entry(left).entry(right).finish()
+            }
+            Element::Number(number) => {
+                f.write_fmt(format_args!("{}",number))
+            }
+        }
+    }
 }
 
 fn parse_number(input: &str) -> IResult<&str, Element> {
@@ -58,6 +72,83 @@ impl FromStr for Element {
     }
 }
 
+impl std::ops::Add for Element {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Element::Pair {
+            left: Box::new(self),
+            right: Box::new(other),
+        }
+    }
+}
+
+impl Element {
+    fn explode_if_necessary(&mut self) -> Option<()> {
+        self.explode_if_nested_four_times(0).map(|_| ())
+    }
+
+    fn increase_left_most_value_by(&mut self, increase: u32) {
+        match self {
+            Element::Pair { left, right: _ } => {
+                left.increase_left_most_value_by(increase);
+            }
+            Element::Number(number) => {
+                *number += increase;
+            }
+        }
+    }
+
+    fn increase_right_most_value_by(&mut self, increase: u32) {
+        match self {
+            Element::Pair { left: _, right } => {
+                right.increase_right_most_value_by(increase);
+            }
+            Element::Number(number) => {
+                *number += increase;
+            }
+        }
+    }
+
+    fn explode_if_nested_four_times(&mut self, nesting: u32) -> Option<(Option<u32>, Option<u32>)> {
+        match self {
+            Element::Pair { left, right } if nesting == 4 => {
+                // explode
+                eprintln!("Explode");
+
+                if let (Element::Number(left_value), Element::Number(right_value)) =
+                    (*left.clone(), *right.clone())
+                {
+                    *self = Element::Number(0);
+                    Some((Some(left_value), Some(right_value)))
+                } else {
+                    panic!("Invalid: nested more than 4 times")
+                }
+            }
+            Element::Pair { left, right } => {
+                if let Some((explode_left, explode_right)) =
+                    left.explode_if_nested_four_times(nesting + 1)
+                {
+                    if let Some(explode_right_value) = explode_right {
+                        right.increase_left_most_value_by(explode_right_value);
+                    }
+                    Some((explode_left, None))
+                } else if let Some((explode_left, explode_right)) =
+                    right.explode_if_nested_four_times(nesting + 1)
+                {
+                    if let Some(explode_left_value) = explode_left {
+                        left.increase_right_most_value_by(explode_left_value);
+                    }
+                    Some((None, explode_right))
+                } else {
+                    None
+                }
+            }
+            Element::Number(_) => None,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -89,6 +180,36 @@ mod tests {
                 }),
                 right: Box::new(Element::Number(3))
             })
+        );
+    }
+
+    #[test]
+    fn test_addition() {
+        assert_eq!(
+            "[1,2]".parse::<Element>().unwrap() + "3".parse::<Element>().unwrap(),
+            Element::Pair {
+                left: Box::new(Element::Pair {
+                    left: Box::new(Element::Number(1)),
+                    right: Box::new(Element::Number(2))
+                }),
+                right: Box::new(Element::Number(3))
+            }
+        );
+    }
+
+    #[test]
+    fn test_exploding() {
+        let mut element = "[[[[[9,8],1],2],3],4]".parse::<Element>().unwrap();
+        assert_eq!(element.explode_if_necessary(), Some(()));
+        assert_eq!(element, "[[[[0,9],2],3],4]".parse().unwrap());
+
+        let mut element = "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"
+            .parse::<Element>()
+            .unwrap();
+        assert_eq!(element.explode_if_necessary(), Some(()));
+        assert_eq!(
+            element,
+            "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]".parse().unwrap()
         );
     }
 }
